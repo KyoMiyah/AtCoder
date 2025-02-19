44e8dcaf483da02cae4b2f3c84ac6b1670b5a27a
Kyohsuke Miyahira <borantia777@yahoo.co.jp>
2025-02-19 18:15:47 +0900

    250218勉強分です。

--- ABC391/d2.cpp
+++ ABC391/d2.cpp
@@ -1,61 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i,n) for (int i=0; i<n; i++)
 using ll = long long;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    int n, w;
+    cin >> n >> w;
+    vector<int> x(n), y(n);
+    rep(i,n) {
+        cin >> x[i] >> y[i];
+        x[i]--;
+    }
+    vector<int> idx(n);
+    rep(i,n) idx[i] = i;
+    sort(idx.begin(), idx.end(), [&](int i, int j) {
+        return y[i] < y[j];
+    });
 
-    int N, W, Q;
-    cin >> N >> W;
-
-    vector<int> X(N), Y(N);
-    vector<pair<int, int>> blocks;
-    for (int i = 0; i < N; i++) {
-        cin >> X[i] >> Y[i];
-        blocks.emplace_back(Y[i], X[i]); // Yを優先してソートするため
+    vector<int> r(n);
+    vector<int> num(w);
+    for (int i : idx) {
+        r[i] = num[x[i]];
+        num[x[i]] ++;
     }
 
-    // 高さが低い順に処理
-    sort(blocks.begin(), blocks.end());
-
-    // 各列の現在のブロックの一番上の位置を管理
-    unordered_map<int, ll> column; // X座標ごとに最大Yを記録
-    for (auto [y, x] : blocks) {
-        if (column.count(x)) {
-            // その列の一番上のブロックのY座標より下に落ちる
-            column[x] = min(column[x] - 1, (ll)y);
-        } else {
-            column[x] = y;
+    vector<int> d(n, 1e9+1);
+    {
+        vector<vector<int>> blocks(n);
+        rep(i,n) blocks[r[i]].push_back(i);
+        rep(i,n) {
+            if (blocks[i].size() != w) continue;
+            int mx = 0;
+            for (int j : blocks[i]) mx = max(mx, y[j]-1);
+            d[i] = mx+1;
         }
     }
 
-    // すべての列の最小値を取得（これが消滅ライン）
-    ll minY = LLONG_MAX;
-    for (auto [x, y] : column) {
-        minY = min(minY, y);
+    int q;
+    cin >> q;
+    rep(qi, q) {
+        int t, a;
+        cin >> t >> a;
+        a--;
+        if (d[r[a]] > t) cout << "Yes" << endl;
+        else cout << "No" << endl;
     }
-
-    // クエリ処理
-    cin >> Q;
-    vector<int> A(Q);
-    vector<ll> T(Q);
-    for (int i = 0; i < Q; i++) {
-        cin >> A[i] >> T[i];
-    }
-
-    // 各クエリに対してブロックの生存確認
-    for (int i = 0; i < Q; i++) {
-        int a = A[i] - 1; // 0-based index
-        ll t = T[i];
-
-        // ブロックが消滅しているか確認
-        if (Y[a] - t <= minY) {
-            cout << "No\n";
-        } else {
-            cout << "Yes\n";
-        }
-    }
-
-    return 0;
+return 0;
 }
